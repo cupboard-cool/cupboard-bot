07e2da7631203f980eaf51bc9f9ccab1f946d50a
kullerQ <kuller.enotik@gmail.com>
2021-04-30 17:46:23 +0200

    Follow and unfollow responses migrated to messages.py

--- functions.py
+++ functions.py
@@ -23,13 +23,13 @@ def process_command(message, chat_id, username, chat_type):
 
     if command == '/try':
         if arg in messages.exceptions_list:
-            message_respone = "У {0}, получилось {1}".format(username, arg)
+            message_response = "У {0}, получилось {1}".format(username, arg)
         elif random() > 0.5:
-            message_respone = "У {0}, получилось {1}".format(username, arg)
+            message_response = "У {0}, получилось {1}".format(username, arg)
         else:
-            message_respone = "У {0}, не получилось {1}".format(username, arg)
+            message_response = "У {0}, не получилось {1}".format(username, arg)
 
-        bot.send_message(chat_id, message_respone)
+        bot.send_message(chat_id, message_response)
 
     if chat_type == 'private':
         if command == '/follow':
@@ -41,7 +41,7 @@ def process_command(message, chat_id, username, chat_type):
 
                     for follower in data['followers']:
                         if follower['chat_id'] == chat_id:
-                            message_respone = "Асыбка! Вы узе падписаны на апавесенние."
+                            message_response = messages.error_follow_response
                             break
                     else:
                         data["followers"].append({'chat_id': chat_id})
@@ -49,16 +49,16 @@ def process_command(message, chat_id, username, chat_type):
                         with open(config.followers_data_path, 'w') as followers_data:
                             json.dump(data, followers_data)
 
-                        message_respone = "Вы успесна падписались на апавесенние."
+                        message_response = messages.successful_follow_response
                 except:
                     pass
 
                 if data == None:
                     with open(config.followers_data_path, 'w') as followers_data:
                         json.dump({"followers": [{'chat_id': chat_id}]}, followers_data)
-                        message_respone = "Вы успесна падписались на апавесенние."
+                        message_response = messages.successful_follow_response
 
-                bot.send_message(chat_id, message_respone)
+                bot.send_message(chat_id, message_response)
 
         if command == '/unfollow':
             data = None
@@ -74,19 +74,19 @@ def process_command(message, chat_id, username, chat_type):
                         with open(config.followers_data_path, 'w') as followers_data:
                             json.dump(data, followers_data)
 
-                        message_respone = "Вы успесна атписались ат апавесенния."
+                        message_response = messages.successful_unfollow_response
                         break
                 else:
-                    message_respone = "Асыбка! Вы исё не падписаны на апавесенние."
+                    message_response = messages.error_unfollow_response
             except:
                 pass
 
             if data == None:
                 with open(config.followers_data_path, 'w') as followers_data:
                     json.dump({"followers": []}, followers_data)
-                    message_respone = "Асыбка! Вы исё не падписаны на апавесенние."
+                    message_response = messages.error_unfollow_response
 
-            bot.send_message(chat_id, message_respone)
+            bot.send_message(chat_id, message_response)
 
 
 
@@ -97,14 +97,14 @@ def process_message(message, chat_id, username, message_words, chat_type):
 
         if trigger_matches or close_trigger_matches:
             if chat_type == 'private':
-                bot.send_message(chat_id, choice(trigger_message_dictionary['respone_message_array']).format(username))
+                bot.send_message(chat_id, choice(trigger_message_dictionary['response_message_array']).format(username))
                 break
             else:
                 name_trigger_matches = [name_trigger for name_trigger in messages.name_triggers if name_trigger in message_words]
                 close_name_trigger_matches = [get_close_matches(name_trigger, message_words) for name_trigger in messages.name_triggers if get_close_matches(name_trigger, message_words)]
                 
                 if name_trigger_matches or close_name_trigger_matches:
-                    bot.send_message(chat_id, choice(trigger_message_dictionary['respone_message_array']).format(username))
+                    bot.send_message(chat_id, choice(trigger_message_dictionary['response_message_array']).format(username))
                     break
     else:
         for nontarget_trigger_message_dictionary in messages.nontarget_trigger_message_dictionaries_list:
@@ -112,5 +112,5 @@ def process_message(message, chat_id, username, message_words, chat_type):
             close_trigger_matches = get_close_matches(message.lower(), nontarget_trigger_message_dictionary['trigger_message_array'], n=1, cutoff=0.7)
 
             if trigger_matches or close_trigger_matches:
-                bot.send_message(chat_id, choice(nontarget_trigger_message_dictionary['respone_message_array']).format(username))
+                bot.send_message(chat_id, choice(nontarget_trigger_message_dictionary['response_message_array']).format(username))
                 break
