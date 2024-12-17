04e20232f3861f1be679bc568eb080348fbcd25a
Kuller <kuller.enotik@gmail.com>
2024-12-17 22:42:51 +0100

    Update with latest changes

--- functions.py
+++ functions.py
@@ -22,12 +22,15 @@ def process_command(message, chat_id, username, chat_type):
     arg = message.split(' ', 1)[1] if len(message.split(' ')) > 1 else None
 
     if command == '/try':
-        if arg in messages.exceptions_list:
-            message_response = "У {0}, получилось {1}".format(username, arg)
-        elif random() > 0.5:
-            message_response = "У {0}, получилось {1}".format(username, arg)
+        for exception in messages.exceptions_list:
+            if arg == exception['text']:
+                message_response = "У {0}, получилось {1}".format(username, arg) if exception['answer'] else "У {0}, не получилось {1}".format(username, arg)
+                break
         else:
-            message_response = "У {0}, не получилось {1}".format(username, arg)
+            if random() > 0.5:
+                message_response = "У {0}, получилось {1}".format(username, arg)
+            else:
+                message_response = "У {0}, не получилось {1}".format(username, arg)
 
         bot.send_message(chat_id, message_response)
 
@@ -102,15 +105,19 @@ def process_message(message, chat_id, username, message_words, chat_type):
             else:
                 name_trigger_matches = [name_trigger for name_trigger in messages.name_triggers if name_trigger in message_words]
                 close_name_trigger_matches = [get_close_matches(name_trigger, message_words) for name_trigger in messages.name_triggers if get_close_matches(name_trigger, message_words)]
-                
+
                 if name_trigger_matches or close_name_trigger_matches:
                     bot.send_message(chat_id, choice(trigger_message_dictionary['response_message_array']).format(username))
                     break
     else:
         for nontarget_trigger_message_dictionary in messages.nontarget_trigger_message_dictionaries_list:
-            trigger_matches = [trigger_message_array_obj for trigger_message_array_obj in nontarget_trigger_message_dictionary['trigger_message_array'] if trigger_message_array_obj in message.lower()]
+            trigger_matches = [{'text': trigger_message_array_obj, 'action': nontarget_trigger_message_dictionary['action']} for trigger_message_array_obj in nontarget_trigger_message_dictionary['trigger_message_array'] if trigger_message_array_obj in message.lower()]
             close_trigger_matches = get_close_matches(message.lower(), nontarget_trigger_message_dictionary['trigger_message_array'], n=1, cutoff=0.7)
 
             if trigger_matches or close_trigger_matches:
-                bot.send_message(chat_id, choice(nontarget_trigger_message_dictionary['response_message_array']).format(username))
-                break
+                if trigger_matches[0]['action'] != 'sneeze':
+                    bot.send_message(chat_id, choice(nontarget_trigger_message_dictionary['response_message_array']).format(username))
+                    break
+                elif get_close_matches(message.lower(), nontarget_trigger_message_dictionary['trigger_message_array'], n=1, cutoff=1):
+                    bot.send_message(chat_id, choice(nontarget_trigger_message_dictionary['response_message_array']).format(username))
+                    break
