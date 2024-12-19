9449a09ae36da8ffe59d22e422a460d35edc16da
Kuller <kuller.enotik@gmail.com>
2024-12-19 01:49:06 +0100

    Remove "key" parameter from config

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ def create_config(config_path):
     config = configparser.ConfigParser()
 
     config['General'] = {
-        'debug': True,
-        'key': ''
+        'debug': 'True'
     }
     config['Bot'] = {
         'token': '',
@@ -29,7 +28,6 @@ def read_config(config_path):
 
     debug_mode = config.getboolean('General', 'debug')
 
-    key = config.get('General', 'key')
     bot_token = config.get('Bot', 'debug_token') if debug_mode \
         else config.get('Bot', 'token')
 
@@ -37,7 +35,6 @@ def read_config(config_path):
 
     config_vals = {
         'debug_mode': debug_mode,
-        'key': key,
         'followers_data_file': followers_data_file,
         'bot_token': bot_token
     }
@@ -53,6 +50,5 @@ if not isfile(config_file):
 config_values = read_config(config_file)
 
 DEBUG_MODE = config_values['debug_mode']
-KEY = config_values['key']
 BOT_TOKEN = config_values['bot_token']
 FOLLOWERS_DATA_FILE = config_values['followers_data_file']
--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from random import choice, getrandbits
+from random import getrandbits
 import json
 from typing import Optional
 
