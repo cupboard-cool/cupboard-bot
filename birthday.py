69d698f51321ec85496715bbfb6085ab596f3d0f
Lotte <lottuce.yami@gmail.com>
2024-12-21 20:03:12 +0100

    Implement mentions without username

--- birthday.py
+++ birthday.py
@@ -26,14 +26,14 @@ def check_birthday(bot: TeleBot) -> None:
 
 
 def congratulate(bot: TeleBot, id: str, birthday: date) -> None:
-    username = bot.get_chat(id).username
+    mention = get_mention(bot, id)
 
-    gift_chat_message = f"У @{username} сегодня день рождения, не забудьте отправить подарки!"
-    bot.send_message(GIFT_CHAT_ID, gift_chat_message)
+    gift_chat_message = f"У {mention} сегодня день рождения, не забудьте отправить подарки!"
+    bot.send_message(GIFT_CHAT_ID, gift_chat_message, "MarkdownV2")
 
-    main_chat_message0 = f"С днём рождения, @{username}!"
+    main_chat_message0 = f"С днём рождения, {mention}!"
     main_chat_message1 = "\U0001F382"
-    bot.send_message(MAIN_CHAT_ID, main_chat_message0)
+    bot.send_message(MAIN_CHAT_ID, main_chat_message0, "MarkdownV2")
     bot.send_message(MAIN_CHAT_ID, main_chat_message1)
 
 
@@ -41,15 +41,28 @@ def ban(bot: TeleBot, id: str) -> None:
     banned = bot.ban_chat_member(GIFT_CHAT_ID, id)
 
     if banned:
-        username = bot.get_chat(id).username
-        message = f"Через {GIFT_PREP_DAYS} дня (или меньше) у @{username} день рождения, поэтому я ЗАБАНИЛ его, чтобы вы смогли в тайне подготовить подарок. Удачи! :)"
-        bot.send_message(GIFT_CHAT_ID, message)
+        mention = get_mention(bot, id)
+        message = f"Через {GIFT_PREP_DAYS} дня (или меньше) у {mention} день рождения, поэтому я ЗАБАНИЛ его, чтобы вы смогли в тайне подготовить подарок. Удачи! :)"
+        bot.send_message(GIFT_CHAT_ID, message, "MarkdownV2")
 
 
 def unban(bot: TeleBot, id: str) -> None:
     unbanned = bot.unban_chat_member(GIFT_CHAT_ID, id)
 
     if unbanned:
-        username = bot.get_chat(id).username
-        message = f"@{username} отметил день рождения и теперь разбанен."
-        bot.send_message(GIFT_CHAT_ID, message)
+        mention = get_mention(bot, id)
+        message = f"{mention} отметил день рождения и теперь разбанен."
+        bot.send_message(GIFT_CHAT_ID, message, "MarkdownV2")
+
+
+def get_mention(bot: TeleBot, id: str) -> str:
+    chat_info = bot.get_chat(id)
+
+    username = chat_info.username
+
+    if username is not None:
+        mention = f"@{username}"
+    else:
+        mention = f"[{chat_info.first_name} {chat_info.last_name}](tg://user?id={id})"
+
+    return mention
