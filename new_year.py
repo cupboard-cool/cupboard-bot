4692cc16ab552adb5314abc16683c95ee0459c34
Lotte <lottuce.yami@gmail.com>
2024-12-30 01:32:05 +0100

    Implement greeting sending

--- new_year.py
+++ new_year.py
@@ -1,6 +1,5 @@
 import json
 from datetime import datetime, UTC, date
-from typing import NoReturn
 
 import schedule
 from telebot import TeleBot
@@ -9,7 +8,7 @@ from config import MAIN_CHAT_ID, TIMEZONES_DATA_FILE
 from functions import get_mention
 
 
-def check_new_year(bot: TeleBot) -> NoReturn:
+def check_new_year(bot: TeleBot) -> None:
     today = datetime.now(UTC).date()
     if today != date(year=today.year, month=12, day=31):
         return
@@ -21,5 +20,17 @@ def check_new_year(bot: TeleBot) -> NoReturn:
         schedule.every().day.at("00:00", tz).do(congratulate, bot=bot, user_ids=uids)
 
 
-def congratulate(bot: TeleBot, user_ids: list[int]) -> NoReturn:
-    raise NotImplementedError
+def congratulate(bot: TeleBot, user_ids: list[int]):
+    mentions: list[str] = []
+    for uid in user_ids:
+        mention = get_mention(bot, uid)
+        if mention is not None:
+            mentions.append(mention)
+    
+    message0 = f"С НОВЫМ ГОДОМ, {", ".join(mentions)}!"
+    message1 = "\U0001F389"
+    
+    bot.send_message(MAIN_CHAT_ID, message0, "HTML")
+    bot.send_message(MAIN_CHAT_ID, message1)
+
+    return schedule.CancelJob
