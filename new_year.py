58d3b00cc1c9ef008b90710d9f87b666276097ac
ロッテ <lottuce.yami@gmail.com>
2025-01-01 19:48:07 +0100

    Add new year module (#8)
    * Add timezones file to config
    
    * Create new year module
    
    * Implement new year greeting scheduling
    
    * Implement greeting sending
    
    * Cancel greeting job without mentions
    
    * Sort imports

---
+++ new_year.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime, UTC, date
+
+import schedule
+from telebot import TeleBot
+
+from functions import get_mention
+from config import MAIN_CHAT_ID, TIMEZONES_DATA_FILE
+
+
+def check_new_year(bot: TeleBot) -> None:
+    today = datetime.now(UTC).date()
+    if today != date(year=today.year, month=12, day=31):
+        return
+    
+    with open(TIMEZONES_DATA_FILE) as file:
+        timezones_data: dict[str, list[int]] = json.load(file)
+    
+    for tz, uids in timezones_data.items():
+        schedule.every().day.at("00:00", tz).do(congratulate, bot=bot, user_ids=uids)
+
+
+def congratulate(bot: TeleBot, user_ids: list[int]):
+    mentions: list[str] = []
+    for uid in user_ids:
+        mention = get_mention(bot, uid)
+        if mention is not None:
+            mentions.append(mention)
+
+    if not mentions:
+        return schedule.CancelJob
+    
+    message0 = f"С НОВЫМ ГОДОМ, {", ".join(mentions)}!"
+    message1 = "\U0001F389"
+    
+    bot.send_message(MAIN_CHAT_ID, message0, "HTML")
+    bot.send_message(MAIN_CHAT_ID, message1)
+
+    return schedule.CancelJob
